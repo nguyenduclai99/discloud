60b8a958a0d3e302eb09a1b1dc635434303f0e2a
nguyenduclai <nguyen.duc.lai@sun-asterisk.com>
2022-07-30 21:56:44 +0700

    update

--- utils/id.js
+++ utils/id.js
@@ -1,4 +1,4 @@
 import crypto from "crypto";
 
 export const randomId = (length = 16) =>
-  crypto.randomBytes(length).toString("hex");
+    crypto.randomBytes(length).toString("hex");
--- utils/stream.js
+++ utils/stream.js
@@ -2,14 +2,14 @@ import { Transform } from "stream";
 
 // https://github.com/forscht/ddrive/blob/3.x/src/utils/asyncStreamProcessor.js
 export class AsyncStreamProcessor extends Transform {
-  constructor(chunkProcessor) {
-    super();
-    this.chunkProcessor = chunkProcessor;
-  }
+    constructor(chunkProcessor) {
+        super();
+        this.chunkProcessor = chunkProcessor;
+    }
 
-  _transform(chunk, encoding, callback) {
-    this.chunkProcessor(chunk)
-      .then(() => callback(null))
-      .catch((err) => callback(err));
-  }
+    _transform(chunk, encoding, callback) {
+        this.chunkProcessor(chunk)
+        .then(() => callback(null))
+        .catch((err) => callback(err));
+    }
 }
